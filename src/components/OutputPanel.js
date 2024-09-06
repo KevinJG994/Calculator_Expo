366f4e348edb052641829fc0afc308e7e2ed51a2
KevinJG994 <kevinjg994@gmail.com>
2024-09-06 17:30:15 +0100

    #02 - Add OutputPanel

---
+++ src/components/OutputPanel.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const OutputPanel = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.outputText}>Output</Text>
+            <Text style={styles.inputText}>0</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+        padding: 20,
+        backgroundColor: '#444',
+    },
+    outputText: {
+        color: '#ff9500',
+        fontSize: 40,
+        position: 'absolute',
+        top: 50,
+        right: 40,
+    },
+    inputText: {
+        color: '#fff',
+        fontSize: 40,
+        position: 'absolute',
+        bottom: 20,
+        right: 40,
+    },
+});
+
+export default OutputPanel;
